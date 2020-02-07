0d1242c4474b4e7ffae9c9bd9349a99b4379b3fe
zyanyalirva <59425380+zyanyalirva@users.noreply.github.com>
2020-02-07 13:00:53 -0600

    prueba cambio

--- src/main/java/hello/service/impl/ServiceImpl.java
+++ src/main/java/hello/service/impl/ServiceImpl.java
@@ -8,8 +8,8 @@ public class ServiceImpl implements Service {
 	@Override
 	public Walmart getInformacion() {
 		Walmart walmart = new Walmart();
-		walmart.setPais("MEX");
-		walmart.setOficina("Azcapotzalco");
+		walmart.setPais("USA");
+		walmart.setOficina("Denver");
 		walmart.setInformacion("POC-WALMEX");
 		return walmart;
 	}
