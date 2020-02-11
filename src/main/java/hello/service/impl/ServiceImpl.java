a5b7cd68f55671f073fb6262dfbc8619c0aede2a
zyanyalirva <59425380+zyanyalirva@users.noreply.github.com>
2020-02-11 15:32:54 -0600

    cambio de pais

--- src/main/java/hello/service/impl/ServiceImpl.java
+++ src/main/java/hello/service/impl/ServiceImpl.java
@@ -8,8 +8,8 @@ public class ServiceImpl implements Service {
 	@Override
 	public Walmart getInformacion() {
 		Walmart walmart = new Walmart();
-		walmart.setPais("FRA");
-		walmart.setOficina("Paris");
+		walmart.setPais("MX");
+		walmart.setOficina("Mexico");
 		walmart.setInformacion("POC-MEX");
 		return walmart;
 	}
